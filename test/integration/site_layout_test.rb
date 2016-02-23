5bc660f95af2de63ee4ce2eb5efeda13c375b46d
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-02-23 16:29:09 +0000

    Added devise and simple form

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,6 +6,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", new_user_session_path
   end
-  
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ Minitest::Reporters.use!
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
