c2c17705e2cc70be40b1e0db4ff26b1384b696c1
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-03-22 18:05:51 +0000

    Added navbar animation when scroll

---
+++ app/assets/javascripts/static_page.js
@@ -0,0 +1,16 @@
+$(".static_pages.home").ready(function() {
+  
+  $(function () {
+	 var $win = $(window);
+	 $win.scroll(function () {
+	 	 $( "header" ).addClass( "navbarlogin" );
+	   if ($win.scrollTop() == 0){
+	       $( "header" ).removeClass( "navbarlogin" );
+	   }
+	 });
+	});
+
+});
+/*
+
+*/
