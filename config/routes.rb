3c6182d27a75ebabdf98a74c7510f9c4a908368d
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-03-28 17:22:33 +0100

    Added company page, changed contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'static_pages#home'
-  get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale 
-  match '/contacts',     to: 'contacts#new',             via: 'get'
-  resources "contacts", only: [:new, :create]
+	devise_for :users
+	root 'static_pages#home'
+	get '/empresa', to: 'static_pages#company'
+
+	get '/change_locale/:locale', to: 'settings#change_locale', as: :change_locale 
+	match '/contacts',     to: 'contacts#new',             via: 'get'
+	resources "contacts", only: [:new, :create]
 end
