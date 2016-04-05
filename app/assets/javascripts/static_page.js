137549febeb4dce7e20cf8c9de70ee844a97bf2f
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2016-04-05 15:33:30 +0100

    Added class when toggle nav on top

--- app/assets/javascripts/static_page.js
+++ app/assets/javascripts/static_page.js
@@ -1,13 +1,24 @@
 $(".static_pages.home").ready(function() {
-  
+  var toggle = false;
+  //when scroll down add class navbarlogin
   $(function () {
 	 var $win = $(window);
 	 $win.scroll(function () {
 	 	 $( "header" ).addClass( "navbarlogin" );
-	   if ($win.scrollTop() == 0){
+	   if ($win.scrollTop() == 0){	
 	       $( "header" ).removeClass( "navbarlogin" );
-	   }
+	   };
 	 });
 	});
-
+  // When toggle nav in the top add class navbarLogin
+		$('#btn-toggle').on('click', function () {
+  		var $win = $(window);
+  		 if ($win.scrollTop() == 0 && toggle === false){	
+  		 		toggle = true;
+	        $( "header" ).addClass( "navbarlogin" );	
+	   	 }else if ($win.scrollTop() == 0){	
+	   	 		toggle = false;
+	   	 		$( "header" ).removeClass( "navbarlogin" );
+	   	 }
+		});
 });
